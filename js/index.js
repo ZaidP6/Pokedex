a50aea5e0b7b267af0cf67acb73e9b9d1d4c3ab9
ZaidP6 <pilarads06@gmail.com>
2024-10-07 12:36:17 +0200

    comentario data-content

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    getPokemonListV2();
+  
+    $(document).on("click", "#btn-get-data", function () {
+      getPokemonListV2();
+    });
+  
+    function getPokemonListV1() {
+      $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon",
+        method: "GET",
+      }).done(function (resp) {
+        var listadoPomemon = resp.results;
+        listadoPomemon.forEach(function (pokemon) {
+          var template = `<p><h1 class="pokemon" pokemonid="1">${pokemon.name}</h1></p>`;
+          $("#data-content").append(template);
+        });
+      });
+    }
+  
+    function getPokemonListV2() {
+      $("#data-content").html("<img src='loading.gif' />");
+      $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon",
+        method: "GET",
+      }).done(function (resp) {
+        setTimeout(function () {
+          $("#data-content").html("");
+          var listadoPomemon = resp.results;
+          listadoPomemon.forEach(function (pokemon) {
+            var pokemonId = pokemon.url.split("/")[6];
+            var template = `<p><a href="detail.html?pid=${pokemonId}"><h1 class="pokemon" pokemonid="1">${pokemon.name}</h1><img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemonId}.png"</a></p>`;
+            $("#data-content").append(template);
+          });
+        }, 1000);
+      });
+    }
+  });
