8c08f1ca7f39abe07b082b92d9659ccfe2a4c239
ZaidP6 <pilarads06@gmail.com>
2024-10-08 10:31:17 +0200

    lista pokemon

--- js/index.js
+++ js/index.js
@@ -19,20 +19,28 @@ $(document).ready(function () {
     }
   
     function getPokemonListV2() {
-      $("#data-content").html("<img src='loading.gif' />");
+      $(".star-container").html("<img src='loading.gif' />");
       $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon",
-        method: "GET",
+          url: "https://pokeapi.co/api/v2/pokemon",
+          method: "GET",
       }).done(function (resp) {
-        setTimeout(function () {
-          $("#data-content").html("");
-          var listadoPomemon = resp.results;
-          listadoPomemon.forEach(function (pokemon) {
-            var pokemonId = pokemon.url.split("/")[6];
-            var template = `<p><a href="detail.html?pid=${pokemonId}"><h1 class="pokemon" pokemonid="1">${pokemon.name}</h1><img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemonId}.png"</a></p>`;
-            $("#data-content").append(template);
-          });
-        }, 1000);
+          setTimeout(function () {
+              $(".star-container").html("");
+              var listadoPomemon = resp.results;
+              listadoPomemon.forEach(function (pokemon) {
+                  var pokemonId = pokemon.url.split("/")[6]; // ID del Pokémon
+                  var template = `
+                    <div class="star">
+                      <a href="detail.html?pid=${pokemonId}">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png" alt="${pokemon.name}" />
+                      </a>
+                    </div>`;
+                  $(".star-container").append(template);
+              });
+          }, 1000);
       });
-    }
-  });
+  }
+  
+  }
+  
+  );
